a765b6b37385c6fbb49f02ee02b085fb87f42e8c
= <=>
2025-03-26 20:30:14 +0100

    remove duplicates from output

--- net101.c
+++ net101.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <ifaddrs.h>
 #include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct name {
+    struct name *next;
+    char name[32];
+} NAME;
 
 void setup_ifa_list(struct ifaddrs **ifa) 
 {
@@ -12,11 +19,43 @@ void setup_ifa_list(struct ifaddrs **ifa)
 
 void print_ifa_names(struct ifaddrs *ifa)
 {
-    struct ifaddrs *iterator;
-    int ifa_counter = 0;
-    for (iterator = ifa; iterator; iterator = iterator->ifa_next) {
-        printf("%2d.\t%s\n", ++ifa_counter, iterator->ifa_name);
+
+    NAME *name_set;
+    name_set = (NAME *)malloc(sizeof(NAME));
+    if (name_set == NULL) {
+        printf("Mem alloc failed\n");
+        exit(2);
     }
+    name_set->next = NULL;
+    struct ifaddrs *iterator;
+    NAME *name_it;
+    int ifa_counter = 0;
+    int headline = 0;
+    for (iterator = ifa; iterator; iterator = iterator->ifa_next) {
+        int add2set = 1;
+        for (name_it = name_set; name_it; name_it = name_it->next) {
+            if (strcmp(name_it->name, iterator->ifa_name) == 0) {
+                add2set = 0;
+                break;
+            }
+        }
+        if (add2set) {
+            NAME *new = (NAME *)malloc(sizeof(NAME));
+            if (new == NULL) {
+                printf("Mem alloc failed\n");
+                exit(2);
+            }
+            strncpy(new->name, iterator->ifa_name, sizeof(new->name));
+            new->next = name_set;
+            name_set = new;
+            if (headline == 0) {
+                printf("\nInterface Names:\n\n");
+                headline = 1;
+            }
+            printf("%4d. %s\n", ++ifa_counter, iterator->ifa_name);
+        }
+    }
+    printf("\n");
 }
 
 int main(void)
@@ -24,8 +63,6 @@ int main(void)
     struct ifaddrs *ifa;
     setup_ifa_list(&ifa);
     print_ifa_names(ifa);
-
     freeifaddrs(ifa);
-    printf("Test: OK\n");
     return 0;
 }
