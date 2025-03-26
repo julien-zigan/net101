20ad37e750388df4fe7ea1430d104ca27faa3ede
= <=>
2025-03-26 18:44:20 +0100

    implement listing of if_addrs names

--- net101.c
+++ net101.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <ifaddrs.h>
+#include <errno.h>
 
 int main(void)
 {
+    struct ifaddrs *ifa;
+    if (getifaddrs(&ifa) == -1) {
+        perror("getifaddrs() failed");
+        return 1;
+    }
+    struct ifaddrs *iterator;
+
+    puts("");
+    int ifa_counter = 0;
+    for (iterator = ifa; iterator; iterator = iterator->ifa_next) {
+        printf("%2d.\t%s\n", ++ifa_counter, iterator->ifa_name);
+    }
+    puts("");
+
+    freeifaddrs(ifa);
     printf("Test: OK\n");
     return 0;
 }
